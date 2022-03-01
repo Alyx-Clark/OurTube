b2c0c5828c027f2d1d5a40537ded90eda7440391
Alyx-Clark <alexclark.edu@gmail.com>
2022-03-01 11:24:49 -0500

    css changes to splash

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ourtubelogo from '../../../app/assets/images/Ourtube.png'
-import signinlogo from '../../../app/assets/images/SignIn.png'
+import signinlogo from '../../../app/assets/images/signinpic.png'
 import profilepic from '../../../app/assets/images/profilepic.png'
+import searchglass from '../../../app/assets/images/searchglass.png'
+import microphone from '../../../app/assets/images/microphone.png'
+import uploadicon from '../../../app/assets/images/uploadicon.png'
+import linkedin from '../../../app/assets/images/linkedin.png'
+import github from '../../../app/assets/images/github.png'
 
 export default ({ currentUser, logout }) => {
 const display = currentUser ? (
@@ -11,20 +16,24 @@ const display = currentUser ? (
     </div>
   ) : (
     <div>
-      {/* <Link to="/login"><img className="signinpic" scr={signinlogo}/></Link> */}
       <Link to="/login"><img className="signinpic" src={signinlogo}/></Link>
     </div>
   );
   return (
     <header className="navs">
-      <div className="display">
-        <h1>{display}</h1>
-      </div>
       <div className="ourLogo">
         <Link to="/"><img className="pic" src={ourtubelogo}/></Link>
       </div>
       <div className="navsearchbar">
-        <input className="searchbar" type="text" placeholder="Search"/>
+        <input type="text" className="searchbar" placeholder="Search"/>
+        <button><img src={searchglass}/></button>
+        <button className="micbtn"><img src={microphone}/></button>
+      </div>
+      <div className="display">
+        <button className="uploadbtn"><img src={uploadicon}/></button>
+        <button className="linkedinbtn"><img src={linkedin}/></button>
+        <button className="githubbtn"><img src={github}/></button>
+        <h1>{display}</h1>
       </div>
     </header>
   )
