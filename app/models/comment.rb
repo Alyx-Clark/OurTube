12c213c3331c88b57b26ac0d419f4bfe1e840f08
Alyx-Clark <alexclark.edu@gmail.com>
2022-03-03 16:07:46 -0500

    changes to video index component

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
 
-    validates :body, :commenter, :video_id, presence: true
+    validates :body, :commenter_id, :video_id, presence: true
 
-    belongs_to :video,
-        primary_key: id,
-        foreign_key: :video_id,
-        class_name: :Video
+    # belongs_to :video,
+    #     primary_key: id,
+    #     foreign_key: :video_id,
+    #     class_name: :Video
 
     belongs_to :commenter,
         primary_key: :id,
