76b4ce9e675de3c421b54d372c0c7813f8947c20
Alyx-Clark <alexclark.edu@gmail.com>
2022-03-03 17:58:33 -0500

    changes to seeds

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :videos, only: [:create, :destroy, :show, :index]
     resources :comments, only: [:create]
-    resources :users, only: [:create]
+    resources :users, only: [:create, :index]
     resource :session, only: [:create, :destroy, :show]
   end
 
